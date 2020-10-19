c5278b8d1fc80e9f908ff190dfeb7d358d14b996
wouterbaltus <wouter@timechimp.com>
2020-10-19 15:17:25 +0200

    Fixed errors

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,10 +1,4 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom';
-
 describe('Thing', () => {
   it('renders without crashing', () => {
-    // const div = document.createElement('div');
-    // ReactDOM.render(<Thing />, div);
-    // ReactDOM.unmountComponentAtNode(div);
   });
 });
