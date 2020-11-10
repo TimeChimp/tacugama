434be5518ad753c3e94e2bdaa37667688fd5e53d
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2020-11-10 09:17:08 +0100

    less stories

--- src/components/input/Input.stories.tsx
+++ src/components/input/Input.stories.tsx
@@ -2,26 +2,13 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import { Input, InputProps } from './';
-import { LabelSmall } from 'baseui/typography';
 
 export default {
   title: 'Components/Input',
   component: Input,
 } as Meta;
 
-const Template: Story<InputProps> = (args) => (
-  <div>
-    <Input {...args} />
-    <Input
-      placeholder="00:00"
-      startEnhancer={<LabelSmall overrides={{ Block: { style: { width: '100px' } } }}>start</LabelSmall>}
-    />
-    <Input
-      placeholder="00:00"
-      startEnhancer={<LabelSmall overrides={{ Block: { style: { width: '100px' } } }}>endendendend</LabelSmall>}
-    />
-  </div>
-);
+const Template: Story<InputProps> = (args) => <Input {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
