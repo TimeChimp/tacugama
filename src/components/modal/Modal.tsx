643ec3febf3fe170d87c018e4fe3ac2fb6e4bbdf
Lex Tas <lextas@users.noreply.github.com>
2020-11-18 10:26:16 +0100

    fix: allow modal overrides

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,26 +1,9 @@
 import React from 'react';
-import { Modal as BaseModal, ModalProps as BaseModelProps, ModalOverrides } from 'baseui/modal';
+import { Modal as BaseModal, ModalProps as BaseModelProps } from 'baseui/modal';
+import { useTheme } from 'hooks';
+import merge from 'deepmerge';
 
 import { border, borderRadius, MountStateNotifier, MountStates } from '../../utils';
-
-const modalOverride = (name?: string): ModalOverrides => ({
-  Dialog: {
-    style: ({ $theme }) => ({
-      ...border($theme.borders.border100),
-      ...borderRadius($theme.borders.radius100),
-    }),
-    props: {
-      'data-testid': `${name ? name + '-' : ''}modal`,
-    },
-  },
-  Close: {
-    style: ({ $theme }) => ({
-      color: $theme.colors.contentTertiary,
-      right: $theme.sizing.scale700,
-      top: $theme.sizing.scale700,
-    }),
-  },
-});
 
 export interface ModalProps extends BaseModelProps {
   children: React.ReactNode;
@@ -28,16 +11,38 @@ export interface ModalProps extends BaseModelProps {
   name?: string;
 }
 
-export const Modal = ({ children, onStateChange, name, ...rest }: ModalProps) => {
+export const Modal = ({ children, onStateChange, name, overrides, ...rest }: ModalProps) => {
   // By utilizing the `MountStateNotifier` inside of `BaseModal` it will know the mountstate for when
   // the modal is closed. The base modal is unmounted, but this component `Modal` is still mounted.
   // Hence using a component to check the current state so the modal can use animations for mount states.
+
+  const { theme } = useTheme();
+
+  const defaultOverrides = {
+    Dialog: {
+      style: {
+        ...border(theme.current.borders.border100),
+        ...borderRadius(theme.current.borders.radius100),
+      },
+      props: {
+        'data-testid': `${name ? name + '-' : ''}modal`,
+      },
+    },
+    Close: {
+      style: {
+        color: theme.current.colors.contentTertiary,
+        right: theme.current.sizing.scale700,
+        top: theme.current.sizing.scale700,
+      },
+    },
+  };
+
   return (
     <BaseModal
       autoFocus={false}
       unstable_ModalBackdropScroll={true} // prop will be removed in the next major version but implemented as the default behavior
+      overrides={merge(overrides || {}, defaultOverrides)}
       {...rest}
-      overrides={modalOverride(name)}
     >
       {onStateChange && <MountStateNotifier onStateChange={onStateChange} />}
       {children}
---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,6 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../providers/ThemeProvider';
+
+export const useTheme = () => useContext(ThemeContext);
+
+export default useTheme;
