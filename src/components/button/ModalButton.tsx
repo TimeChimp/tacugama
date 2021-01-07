95ca0c855bebd0a5a41423a803ddf3a8f9a24e81
Hidde Ottens <hidde95@gmail.com>
2021-01-07 11:31:37 +0100

    Fix modal button loading state

--- src/components/button/ModalButton.tsx
+++ src/components/button/ModalButton.tsx
@@ -1,22 +1,59 @@
 import React from 'react';
 import { ModalButton as BaseModalButton } from 'baseui/modal';
 import { ButtonProps as BaseButtonProps } from 'baseui/button';
-import { borderRadius } from '../../utils';
+import { borderBottom, borderLeft, borderRadius, borderRight, borderTop } from '../../utils';
+import useTheme from '../../providers/ThemeProvider';
 
-export const ModalButton = ({ children, size = 'compact', ...rest }: BaseButtonProps) => (
-  <BaseModalButton
-    size={size}
-    overrides={{
-      BaseButton: {
-        style: ({ $theme }) => ({
-          ...borderRadius($theme.borders.radius100),
-        }),
+export const ModalButton = ({ children, size = 'compact', ...rest }: BaseButtonProps) => {
+  const {
+    theme: {
+      current: {
+        sizing: { scale0, scale600 },
+        colors: { primaryB, borderTransparent },
       },
-    }}
-    {...rest}
-  >
-    {children}
-  </BaseModalButton>
-);
+    },
+  } = useTheme();
+  return (
+    <BaseModalButton
+      size={size}
+      overrides={{
+        BaseButton: {
+          style: ({ $theme }) => ({
+            ...borderRadius($theme.borders.radius100),
+          }),
+        },
+        LoadingSpinner: {
+          style: {
+            width: scale600,
+            height: scale600,
+            ...borderBottom({
+              borderWidth: scale0,
+              borderStyle: 'solid',
+              borderColor: primaryB,
+            }),
+            ...borderLeft({
+              borderWidth: scale0,
+              borderStyle: 'solid',
+              borderColor: primaryB,
+            }),
+            ...borderRight({
+              borderWidth: scale0,
+              borderStyle: 'solid',
+              borderColor: primaryB,
+            }),
+            ...borderTop({
+              borderWidth: scale0,
+              borderStyle: 'solid',
+              borderColor: borderTransparent,
+            }),
+          },
+        },
+      }}
+      {...rest}
+    >
+      {children}
+    </BaseModalButton>
+  );
+};
 
 export default ModalButton;
