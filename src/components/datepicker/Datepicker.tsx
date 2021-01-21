e6b0811c5004fbb53cf3be656f12aa5ac8ecb57b
wouterbaltus <wouter@timechimp.com>
2021-01-21 17:25:14 +0100

    Added datepicker and updated select

---
+++ src/components/datepicker/Datepicker.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Popover } from '../popover';
+import { TetherPlacement } from 'baseui/layer';
+import { borderBottom } from '../../utils';
+import { useTheme } from 'providers';
+import { ClickOutside } from '../click-outside/ClickOutside';
+import { StatefulCalendar } from 'baseui/datepicker';
+
+export interface DatepickerProps {
+  date: Date;
+  placement?: TetherPlacement[keyof TetherPlacement];
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => any;
+  onChange?: (date: Date) => any;
+}
+
+export const Datepicker = ({ date, placement = 'bottomLeft', setIsOpen, isOpen }: DatepickerProps) => {
+  const {
+    theme: {
+      current: {
+        borders: { border300 },
+        colors: { primaryA, primaryB, contentTertiary },
+      },
+    },
+  } = useTheme();
+
+  return (
+    <Popover
+      isOpen={isOpen}
+      placement={placement}
+      content={() => (
+        <ClickOutside onClickOutside={() => isOpen && setIsOpen(false)}>
+          <StatefulCalendar
+            value={date}
+            overrides={{
+              CalendarHeader: {
+                style: {
+                  backgroundColor: primaryB,
+                  ...borderBottom(border300),
+                },
+              },
+              MonthHeader: {
+                style: {
+                  backgroundColor: primaryB,
+                  color: primaryA,
+                  fontWeight: 600,
+                },
+              },
+              MonthYearSelectButton: {
+                style: {
+                  color: primaryA,
+                  fontWeight: 600,
+                },
+              },
+              PrevButton: {
+                style: {
+                  color: contentTertiary,
+                },
+              },
+              NextButton: {
+                style: {
+                  color: contentTertiary,
+                },
+              },
+            }}
+          />
+        </ClickOutside>
+      )}
+    >
+      <div />
+    </Popover>
+  );
+};
