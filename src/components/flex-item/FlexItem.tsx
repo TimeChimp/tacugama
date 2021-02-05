c8ef7e48ae6a54d3e9cc62501cb5c766059a3656
Hidde Ottens <hidde95@gmail.com>
2021-02-05 10:51:23 +0100

    Add flex item component

---
+++ src/components/flex-item/FlexItem.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { TextAlignProperty } from '../../models';
+import { StyledItem } from './StyledFlexItem';
+
+export interface FlexItemProps {
+  children: React.ReactNode;
+  justifyContent?: string;
+  alignItems?: string;
+  textAlign?: TextAlignProperty;
+  width?: string;
+  marg1?: string;
+  marg2?: string;
+  marg3?: string;
+  marg4?: string;
+}
+
+export const FlexItem = ({
+  children,
+  justifyContent,
+  alignItems,
+  width,
+  textAlign,
+  marg1,
+  marg2,
+  marg3,
+  marg4,
+}: FlexItemProps) => {
+  return (
+    <StyledItem
+      $justifyContent={justifyContent}
+      $alignItems={alignItems}
+      $width={width}
+      $textAlign={textAlign}
+      $marg1={marg1}
+      $marg2={marg2}
+      $marg3={marg3}
+      $marg4={marg4}
+    >
+      {children}
+    </StyledItem>
+  );
+};
