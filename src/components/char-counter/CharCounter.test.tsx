e21461bdc6db02627b5376e48eebfd5eb05f9cb8
Hidde Ottens <hidde95@gmail.com>
2021-02-09 15:49:53 +0100

    Add charcounter unit test

--- src/components/char-counter/CharCounter.test.tsx
+++ src/components/char-counter/CharCounter.test.tsx
@@ -1,6 +1,6 @@
-import '@testing-library/jest-dom'
-import * as React from 'react'
-import {render, screen} from '../../utils/test-utils'
+import '@testing-library/jest-dom';
+import * as React from 'react';
+import { render, screen } from '../../utils/test-utils';
 import { CharCounter } from '.';
 
 const TITLE = 'test';
@@ -8,13 +8,13 @@ const CHAR_COUNT = 60;
 const LABEL_TEXT = `${TITLE} (${CHAR_COUNT})`;
 
 test('it shows number of characters when error', () => {
-  render(<CharCounter error charCount={CHAR_COUNT} title={TITLE} />)
+  render(<CharCounter error charCount={CHAR_COUNT} title={TITLE} />);
 
-  expect(screen.getAllByText(LABEL_TEXT)).toBeInTheDocument()
-})
+  expect(screen.getAllByText(LABEL_TEXT)).toBeInTheDocument();
+});
 
 test('it shows no characters when no error', () => {
-  render(<CharCounter error={false} charCount={CHAR_COUNT} title={TITLE} />)
+  render(<CharCounter error={false} charCount={CHAR_COUNT} title={TITLE} />);
 
-  expect(screen.queryAllByText(LABEL_TEXT)).not.toBeInTheDocument()
+  expect(screen.queryAllByText(LABEL_TEXT)).not.toBeInTheDocument();
 });
