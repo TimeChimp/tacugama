d7b1092c3720b0a41267c1c481fa0ecdbc047763
Hidde Ottens <hidde95@gmail.com>
2021-02-09 15:49:40 +0100

    Add avatar tests

--- src/components/avatar/Avatar.test.tsx
+++ src/components/avatar/Avatar.test.tsx
@@ -1,24 +1,24 @@
-import '@testing-library/jest-dom'
-import * as React from 'react'
-import {render, screen} from '../../utils/test-utils'
-import { Avatar} from '.';
-
+import '@testing-library/jest-dom';
+import * as React from 'react';
+import { render, screen } from '../../utils/test-utils';
+import { Avatar } from '.';
 
 test('it shows initials', () => {
   const testName = 'John Deo';
   const intitials = 'JD';
 
-  render(<Avatar name={testName} />)
+  render(<Avatar name={testName} />);
 
-  expect(screen.getByText(intitials)).toBeInTheDocument()
+  expect(screen.getByText(intitials)).toBeInTheDocument();
 });
 
 test('it shows no initials when supplying an image src', () => {
   const testName = 'John Deo';
   const intitials = 'JD';
-  const avatarLink = 'https://st.depositphotos.com/3244893/5106/v/950/depositphotos_51060005-stock-illustration-cute-little-panda-icon.jpg'
+  const avatarLink =
+    'https://st.depositphotos.com/3244893/5106/v/950/depositphotos_51060005-stock-illustration-cute-little-panda-icon.jpg';
 
-  render(<Avatar name={testName} src={avatarLink} />)
+  render(<Avatar name={testName} src={avatarLink} />);
 
-  expect(screen.queryByText(intitials)).not.toBeInTheDocument()
-})
+  expect(screen.queryByText(intitials)).not.toBeInTheDocument();
+});
--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,20 +1,14 @@
-import React, { FC, ReactElement } from 'react'
-import { render, RenderOptions } from '@testing-library/react'
+import React, { FC, ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
 import { ThemeProvider } from '../providers';
 
 const Providers: FC = ({ children }) => {
-  return (
-    <ThemeProvider>
-        {children}
-    </ThemeProvider>
-  )
-}
+  return <ThemeProvider>{children}</ThemeProvider>;
+};
 
-const customRender = (
-  ui: ReactElement,
-  options?: Omit<RenderOptions, 'queries'>
-) => render(ui, { wrapper: Providers, ...options })
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'queries'>) =>
+  render(ui, { wrapper: Providers, ...options });
 
-export * from '@testing-library/react'
+export * from '@testing-library/react';
 
-export { customRender as render }
+export { customRender as render };
