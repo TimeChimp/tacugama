e404f411efe1c39f74c03f8821e1c6787d3b2d5a
Hidde Ottens <hidde95@gmail.com>
2021-02-10 12:31:33 +0100

    Add account icons

--- src/components/icons/icons.stories.tsx
+++ src/components/icons/icons.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import { TcClock } from './';
+import { TcClock, Account as AccountIcon, Lock as LockIcon, Flag as FlagIcon } from './';
 import { SVGProps } from './Icon';
 
 export default {
@@ -9,9 +9,19 @@ export default {
   component: TcClock,
 } as Meta;
 
-const Template: Story<SVGProps> = (args) => <TcClock {...args} />;
+const ClockTemplate: Story<SVGProps> = (args) => <TcClock {...args} />;
+const AccountTemplate: Story<SVGProps> = (args) => <AccountIcon {...args} />;
+const LockTemplate: Story<SVGProps> = (args) => <LockIcon {...args} />;
+const FlagTemplate: Story<SVGProps> = (args) => <FlagIcon {...args} />;
 
-export const Clock = Template.bind({});
-Clock.args = {
-  size: '50',
-};
+export const Clock = ClockTemplate.bind({});
+Clock.args = {};
+
+export const Account = AccountTemplate.bind({});
+Clock.args = {};
+
+export const Lock = LockTemplate.bind({});
+Clock.args = {};
+
+export const Flag = FlagTemplate.bind({});
+Clock.args = {};
