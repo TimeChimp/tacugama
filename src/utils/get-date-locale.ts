010ce774bce4d07230e4c07bf6de293c91a0fe48
Hidde Ottens <hidde95@gmail.com>
2021-02-17 12:02:21 +0100

    feat: add en-gb as supported language

--- src/utils/get-date-locale.ts
+++ src/utils/get-date-locale.ts
@@ -1,11 +1,13 @@
 import { SupportedLocale } from '../types/SupportedLocale';
 import nl from 'date-fns/locale/nl';
 import enUS from 'date-fns/locale/en-US';
+import enGB from 'date-fns/locale/en-GB';
 
 export const getDateLocale = (locale: SupportedLocale) => {
   const locales = {
     'nl-NL': nl,
     'en-US': enUS,
+    'en-GB': enGB,
   };
 
   return locales[locale];
