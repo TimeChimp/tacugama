98e45ec1b916050b220ce6e26644b5dc9b111086
Hidde Ottens <hidde95@gmail.com>
2021-02-18 17:18:20 +0100

    fix: add noBorder prop to box

--- src/components/box/Box.tsx
+++ src/components/box/Box.tsx
@@ -3,14 +3,18 @@ import { Block, BlockProps } from 'baseui/block';
 import { ThemeProps } from 'models';
 import { border, borderRadius } from '../../utils';
 
-export const Box = ({ ...rest }: BlockProps) => {
+export interface BoxProps extends BlockProps {
+  noBorder?: boolean;
+}
+
+export const Box = ({ noBorder = false, ...rest }: BoxProps) => {
   return (
     <Block
       {...rest}
       overrides={{
         Block: {
           style: ({ $theme }: ThemeProps) => ({
-            ...border($theme.borders.border300),
+            ...border(!noBorder ? $theme.borders.border300 : undefined),
             ...borderRadius($theme.borders.radius100),
             background: $theme.colors.primaryB,
           }),
