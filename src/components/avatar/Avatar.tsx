d7e773a9369b905d045108bfcdc7567a20552d1c
Hidde Ottens <hidde95@gmail.com>
2021-03-03 09:30:59 +0100

    fix: update avatar background color

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,29 +1,45 @@
 import React from 'react';
 import { Avatar as BaseAvatar, AvatarProps as BaseAvatarProps } from 'baseui/avatar';
+import { Theme } from 'baseui/theme';
 import { DATA_TEST_ID } from '../../models';
 
 export interface AvatarProps extends BaseAvatarProps {
   testId?: string;
 }
 
-export const Avatar = ({ testId, ...rest }: AvatarProps) => (
-  <BaseAvatar
-    overrides={{
-      Root: {
-        style: ({ $theme }) => ({
-          fontSize: $theme.sizing.scale800,
-          backgroundColor: '#F6C824',
-        }),
-        props: {
-          [DATA_TEST_ID]: testId,
+export const Avatar = ({ testId, size, ...rest }: AvatarProps) => {
+  const getFontSize = ($theme: Theme) => {
+    if (size) {
+      const sizeNumber = parseInt(size, 10);
+      const defaultSize = parseInt($theme.sizing.scale1000, 10);
+
+      if (sizeNumber > defaultSize) {
+        return $theme.sizing.scale800;
+      }
+    }
+    return $theme.sizing.scale550;
+  };
+
+  return (
+    <BaseAvatar
+      overrides={{
+        Root: {
+          style: ({ $theme }) => ({
+            backgroundColor: $theme.colors.primary,
+          }),
+          props: {
+            [DATA_TEST_ID]: testId,
+          },
         },
-      },
-      Initials: {
-        style: ({ $theme }) => ({
-          color: $theme.colors.primaryA,
-        }),
-      },
-    }}
-    {...rest}
-  />
-);
+        Initials: {
+          style: ({ $theme }) => ({
+            fontSize: getFontSize($theme),
+            color: $theme.colors.primaryA,
+          }),
+        },
+      }}
+      {...rest}
+      size={size}
+    />
+  );
+};
