ba73adf7216c5329e3a1a0545bc90275e0bc04c6
Bohdan Burukhin <b.buruhkin@timechimp.com>
2021-06-03 16:52:02 +0300

    fix: updated Edit row cell click handling

--- src/components/data-grid/RowEditCell.tsx
+++ src/components/data-grid/RowEditCell.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useTheme } from '../../providers';
 import { Pencil } from '../icons';
 import { TertiaryButton } from '../button';
@@ -13,10 +13,17 @@ export const RowEditCell = ({ onRowEdit }: RowEditCellProps) => {
       },
     },
   } = useTheme();
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const onButtonClick = () => {
+    // Simulate click on ag-grid row to prevent current row not being selected when clicking directly on the action menu icon
+    containerRef.current?.click();
+    onRowEdit();
+  };
 
   return (
-    <div className="ag-row-edit-cell">
-      <TertiaryButton onClick={onRowEdit}>
+    <div className="ag-row-edit-cell" ref={containerRef} onClick={onButtonClick}>
+      <TertiaryButton>
         <Pencil size={scale500} color={contentTertiary} />
       </TertiaryButton>
     </div>
