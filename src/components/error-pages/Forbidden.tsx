eeea621ec0300512cf02d774005452d84285f6b8
Bohdan Burukhin <b.buruhkin@timechimp.com>
2021-06-23 15:48:45 +0300

    feat(CSR-1435): created Forbidden error page

--- src/components/error-pages/Forbidden.tsx
+++ src/components/error-pages/Forbidden.tsx
@@ -1,11 +1,24 @@
-import { HeadingSmall } from '../typography';
 import React from 'react';
+import ErrorPage from './ErrorPage';
+import { ErrorPageProps } from './types';
 
-export const Forbidden = () => {
+export const Forbidden = ({
+  title,
+  description,
+  primaryButtonText,
+  secondaryButtonText,
+  onPrimaryButtonClick,
+  onSecondaryButtonClick,
+}: ErrorPageProps) => {
   return (
-    <div>
-      <HeadingSmall>403 - Forbidden</HeadingSmall>
-    </div>
+    <ErrorPage
+      title={title}
+      description={description}
+      primaryButtonText={primaryButtonText}
+      secondaryButtonText={secondaryButtonText}
+      onPrimaryButtonClick={onPrimaryButtonClick}
+      onSecondaryButtonClick={onSecondaryButtonClick}
+    />
   );
 };
 
