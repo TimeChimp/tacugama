19c64a3095f14bb28c91013eb7f0c58f5a20fefc
Hidde Ottens <hidde95@gmail.com>
2022-01-14 14:45:39 +0100

    fix: prettier issue

--- src/components/dnd-list/DndList.tsx
+++ src/components/dnd-list/DndList.tsx
@@ -1,22 +1,14 @@
-import * as React from "react";
-import {
-  List,
-  arrayMove,
-  arrayRemove
-} from "baseui/dnd-list";
-import { DndListProps } from "./types";
+import * as React from 'react';
+import { List, arrayMove, arrayRemove } from 'baseui/dnd-list';
+import { DndListProps } from './types';
 
 export const DndList = ({ items, setItems }: DndListProps) => {
   return (
     <List
       items={items}
       onChange={({ oldIndex, newIndex }) =>
-        setItems(
-          newIndex === -1
-            ? arrayRemove(items, oldIndex)
-            : arrayMove(items, oldIndex, newIndex)
-        )
+        setItems(newIndex === -1 ? arrayRemove(items, oldIndex) : arrayMove(items, oldIndex, newIndex))
       }
     />
   );
-}
+};
