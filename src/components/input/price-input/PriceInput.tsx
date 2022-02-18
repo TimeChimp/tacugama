76fcce1fd0befb1f2849c74ef15a024cede7a17a
Hidde Ottens <hidde95@gmail.com>
2022-02-18 18:06:30 +0100

    feat: add price input component

--- src/components/input/price-input/PriceInput.tsx
+++ src/components/input/price-input/PriceInput.tsx
@@ -1,22 +1,50 @@
+import { NumberFormat } from '@timechimp/timechimp-typescript-helpers';
 import React from 'react';
 import { PRICE_INPUT_PLACEHOLDER, PRICE_INPUT_PREFIX } from '../../../models';
 import { PriceInputComponent } from './styles';
 import { PriceInputProps } from './types';
 
+const getNumberSeparators = (numberFormat: NumberFormat) => {
+  switch (numberFormat) {
+    case '1.234,56':
+      return { thousandSeparator: '.', decimalSeparator: ',' };
+    case '1 234,56':
+      return { thousandSeparator: ' ', decimalSeparator: ',' };
+    case '1,234.56':
+    default:
+      return { thousandSeparator: ',', decimalSeparator: '.' };
+  }
+};
+
 export const PriceInput = ({
   placeholder = PRICE_INPUT_PLACEHOLDER,
   prefix = PRICE_INPUT_PREFIX,
-  thousandSeparator = true,
   allowNegative = true,
   error,
   onChange,
-}: PriceInputProps) => (
-  <PriceInputComponent
-    $error={error}
-    placeholder={placeholder}
-    prefix={prefix}
-    thousandSeparator={thousandSeparator}
-    allowNegative={allowNegative}
-    onValueChange={({ value }) => onChange(value)}
-  />
-);
+  disabled,
+  defaultValue,
+  onKeyPress,
+  onBlur,
+  numberFormat = '1,234.56',
+  testId = 'price-input',
+}: PriceInputProps) => {
+  const { thousandSeparator, decimalSeparator } = getNumberSeparators(numberFormat);
+  return (
+    <PriceInputComponent
+      $error={error}
+      placeholder={placeholder}
+      prefix={prefix}
+      allowNegative={allowNegative}
+      onValueChange={({ value }) => onChange(value)}
+      disabled={disabled}
+      defaultValue={defaultValue}
+      onKeyPress={onKeyPress}
+      onBlur={onBlur}
+      autoComplete="off"
+      thousandSeparator={thousandSeparator}
+      decimalSeparator={decimalSeparator}
+      data-test-id={testId}
+    />
+  );
+};
--- src/components/input/price-input/types.ts
+++ src/components/input/price-input/types.ts
@@ -1,3 +1,4 @@
+import { NumberFormat } from '@timechimp/timechimp-typescript-helpers';
 import { NumberFormatProps } from 'react-number-format';
 
 export interface PriceInputComponentProps extends NumberFormatProps {
@@ -9,4 +10,6 @@ type OmitValues = 'onChange' | 'onValueChange';
 export interface PriceInputProps extends Omit<NumberFormatProps, OmitValues> {
   error?: boolean | undefined;
   onChange: (value: string) => void;
+  numberFormat?: NumberFormat;
+  testId?: string;
 }
