57580531aecd961bd1f9c32fea9161e8c911ec28
Ihor Lavrov <40561417+Ihor-Lavrov@users.noreply.github.com>
2022-04-12 10:22:16 +0300

    feat(CSR-2676) add absence bar (#707)

--- src/components/graphs/myDashboardBar/myDashboardBar.stories.tsx
+++ src/components/graphs/myDashboardBar/myDashboardBar.stories.tsx
@@ -15,210 +15,262 @@ MyDashboardBarDefault.args = {
     {
       isoWeek: 1,
       duration: 0,
+      absence: 3600,
     },
     {
       isoWeek: 2,
       duration: 0,
+      absence: 144000,
     },
     {
       isoWeek: 3,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 4,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 5,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 6,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 7,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 8,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 9,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 10,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 11,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 12,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 13,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 14,
       duration: 0,
+      absence: 24000,
     },
     {
       isoWeek: 15,
       duration: 0,
+      absence: 180000,
     },
     {
       isoWeek: 16,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 17,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 18,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 19,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 20,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 21,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 22,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 23,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 24,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 25,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 26,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 27,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 28,
       duration: 0,
+      absence: 194400,
     },
     {
       isoWeek: 29,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 30,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 31,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 32,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 33,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 34,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 35,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 36,
       duration: 21600,
+      absence: 0,
     },
     {
       isoWeek: 37,
       duration: 43200,
+      absence: 28800,
     },
     {
       isoWeek: 38,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 39,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 40,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 41,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 42,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 43,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 44,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 45,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 46,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 47,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 48,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 49,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 50,
       duration: 0,
+      absence: 0,
     },
     {
       isoWeek: 51,
       duration: 43200,
+      absence: 0,
     },
     {
       isoWeek: 52,
       duration: 144000,
+      absence: 0,
     },
   ],
 };
