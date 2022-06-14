370a2775c689349eb7b927f5114529448f3af75d
Hidde Ottens <hidde95@gmail.com>
2022-06-14 09:52:59 +0200

    fix: secondary button styling (#755)
    * fix: secondary button styling
    
    * chore: bump version

--- src/components/button/secondary-button/SecondaryButton.tsx
+++ src/components/button/secondary-button/SecondaryButton.tsx
@@ -1,32 +1,91 @@
 import React, { forwardRef } from 'react';
-import { Button, ButtonProps } from '../Button';
-import { border, borderRadius } from '../../../utils';
-
-export interface SecondaryButtonProps extends ButtonProps {
-  children?: React.ReactNode;
-}
+import { Button } from '../Button';
+import {
+  border,
+  borderBottom,
+  borderLeft,
+  borderRadius,
+  borderRight,
+  borderTop,
+  margin,
+  padding,
+} from '../../../utils';
+import { SecondaryButtonProps } from './types';
+import { useTheme } from '../../../providers';
 
 export const SecondaryButton = forwardRef<HTMLButtonElement, SecondaryButtonProps>(
-  ({ children, ...rest }: SecondaryButtonProps, ref) => (
-    <Button
-      ref={ref}
-      kind="secondary"
-      overrides={{
-        BaseButton: {
-          style: ({ $theme }) => {
-            return {
-              ...border($theme.borders.border300),
-              boxSizing: 'border-box',
-              ...borderRadius($theme.borders.radius200),
-            };
-          },
+  ({ children, ...rest }: SecondaryButtonProps, ref) => {
+    const {
+      theme: {
+        current: {
+          sizing: { scale0, scale100, scale200, scale600 },
+          borders: { radius200, border300 },
+          colors,
         },
-      }}
-      {...rest}
-    >
-      {children}
-    </Button>
-  ),
+      },
+    } = useTheme();
+    const { primaryB, primary400, borderTransparent } = colors;
+
+    return (
+      <Button
+        ref={ref}
+        kind="secondary"
+        overrides={{
+          Root: {
+            style: {
+              ...borderRadius(radius200),
+              ...padding(scale200, scale600),
+              fontWeight: 'normal',
+              backgroundColor: 'transparent',
+              ...border({
+                ...border300,
+                borderColor: primary400,
+              }),
+              color: primary400,
+              ':hover': {
+                backgroundColor: primary400,
+                color: primaryB,
+              },
+            },
+          },
+          StartEnhancer: {
+            style: {
+              ...margin('0', scale100, '0', '0'),
+            },
+          },
+          LoadingSpinner: {
+            style: {
+              width: scale600,
+              height: scale600,
+              ...borderBottom({
+                borderWidth: scale0,
+                borderStyle: 'solid',
+                borderColor: primaryB,
+              }),
+              ...borderLeft({
+                borderWidth: scale0,
+                borderStyle: 'solid',
+                borderColor: primaryB,
+              }),
+              ...borderRight({
+                borderWidth: scale0,
+                borderStyle: 'solid',
+                borderColor: primaryB,
+              }),
+              ...borderTop({
+                borderWidth: scale0,
+                borderStyle: 'solid',
+                borderColor: borderTransparent,
+              }),
+            },
+          },
+        }}
+        {...rest}
+      >
+        {children}
+      </Button>
+    );
+  },
 );
 
 export default SecondaryButton;
