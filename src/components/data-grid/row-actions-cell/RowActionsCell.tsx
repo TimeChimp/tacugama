36aec05f9935c335df1ec320314fa3504309a82c
Hidde Ottens <hidde95@gmail.com>
2022-07-27 14:04:48 +0200

    fix: data grid styling issues

--- src/components/data-grid/row-actions-cell/RowActionsCell.tsx
+++ src/components/data-grid/row-actions-cell/RowActionsCell.tsx
@@ -43,23 +43,25 @@ export const RowActionsCell = ({ data }: RowActionsCellProps) => {
     });
   }, [items, data]);
 
-  return !onEdit ? (
-    <div ref={containerRef}>
-      <Dropdown
-        onOpen={onOpen}
-        onClose={() => setActive(false)}
-        items={filteredItems}
-        selectedIds={[id]}
-        additionalProperties={api}
-      >
-        <TertiaryButton type="button">
-          {active ? <ActionMenuActive size={scale500} /> : <ActionMenu size={scale500} />}
-        </TertiaryButton>
-      </Dropdown>
-    </div>
-  ) : (
-    <RowEditCell onClick={handleEdit} icon={icon} />
-  );
+  // return !onEdit ? (
+  //   <div ref={containerRef}>
+  //     <Dropdown
+  //       onOpen={onOpen}
+  //       onClose={() => setActive(false)}
+  //       items={filteredItems}
+  //       selectedIds={[id]}
+  //       additionalProperties={api}
+  //     >
+  //       <TertiaryButton type="button">
+  //         {active ? <ActionMenuActive size={scale500} /> : <ActionMenu size={scale500} />}
+  //       </TertiaryButton>
+  //     </Dropdown>
+  //   </div>
+  // ) : (
+  //   <RowEditCell onClick={handleEdit} icon={icon} />
+  // );
+
+  return null;
 };
 
 export default RowActionsCell;
