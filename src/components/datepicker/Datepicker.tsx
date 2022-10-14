ed802cf9dbf465f6f91ec82af2a0a067f65a2855
Hidde Ottens <hidde95@gmail.com>
2022-10-14 10:17:21 +0200

    feat: update supported languages

--- src/components/datepicker/Datepicker.tsx
+++ src/components/datepicker/Datepicker.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Popover } from '../popover';
 import { TetherPlacement } from 'baseui/layer';
-import { borderBottom, getDateLocale } from '../../utils';
+import { borderBottom } from '../../utils';
 import { useTheme } from '../../providers';
 import { CalendarProps, Calendar } from 'baseui/datepicker';
-import { SupportedLocale } from '../../types';
 import { Select } from '../select';
+import { getDateLocale, SupportedLocale } from '@timechimp/timechimp-typescript-helpers';
 
 export interface DatepickerProps extends CalendarProps {
   date?: Date | Date[];
