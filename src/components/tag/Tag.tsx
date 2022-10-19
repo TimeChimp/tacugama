ad2ae38d474b11af15916114a93f511b67939daf
Olha <77673057+ohalytska-ui@users.noreply.github.com>
2022-10-19 11:41:57 +0200

    feat(CSR-3318): ta refactor tag (#846)
    * feat(CSR-3318): ta refactor tag
    
    * fix: fixed small issues
    
    * fix: fixed small issues with color
    
    * fix: fixed bug
    
    Co-authored-by: ohalytska-ui <o.halytska@timechimp.com>

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -1,39 +1,105 @@
 import React, { ReactNode } from 'react';
-import { ParagraphSmall } from 'baseui/typography';
+import { Tag as TagComponent, TagProps as TagComponentProps } from 'baseui/tag';
 import { useTheme } from '../../providers';
-import { borderRadius, padding } from '../../utils';
+import { borderRadius, margin, padding } from '../../utils';
 
-export interface TagProps {
-  value?: string;
-  children?: ReactNode;
+export enum TagSize {
+  small = 'small',
+  large = 'large',
 }
 
-export const Tag = ({ value }: TagProps) => {
+export interface TagProps extends TagComponentProps {
+  value?: string;
+  children?: ReactNode;
+  size?: TagSize;
+  closeable?: boolean;
+}
+
+export const Tag = ({ value, size = TagSize.small, closeable = false, ...rest }: TagProps) => {
   const {
     theme: {
       current: {
-        sizing: { scale0, scale300 },
-        customColors: { light2 },
-        borders: { radius200 },
+        sizing: { scale0, scale100, scale200, scale300, scale400, scale500, scale550, scale700 },
+        customColors: { light2, dark1 },
       },
     },
   } = useTheme();
 
   return (
-    <ParagraphSmall
+    <TagComponent
+      {...rest}
+      closeable={closeable}
+      size={size}
       overrides={{
-        Block: {
-          style: {
-            display: 'inline-flex',
-            background: light2,
-            margin: scale0,
-            ...padding(scale0, scale300),
-            ...borderRadius(radius200),
+        ...(size === TagSize.small && {
+          Root: {
+            style: () => ({
+              display: 'inline-flex',
+              ...padding('0px', scale200),
+              ...borderRadius(scale100),
+              ...margin('0px', scale100, '0px', '0px'),
+              border: `1px solid ${light2}`,
+              background: '#F9FAFB', // NOTE: Value does not exist in theme
+            }),
           },
-        },
+          Text: {
+            style: () => ({
+              fontSize: scale500,
+              lineHeight: scale700,
+              fontWeight: 400,
+              color: dark1,
+            }),
+          },
+          Action: {
+            style: () => ({
+              marginLeft: scale100,
+            }),
+          },
+          ActionIcon: {
+            style: () => ({
+              height: scale300,
+              width: scale300,
+              color: dark1,
+            }),
+          },
+        }),
+        ...(size === TagSize.large && {
+          Root: {
+            style: () => ({
+              display: 'inline-flex',
+              height: 'auto',
+              ...padding(scale0, scale300),
+              ...borderRadius(scale100),
+              ...margin('0px', scale100, '0px', '0px'),
+              border: `1px solid ${light2}`,
+              background: '#F9FAFB', // NOTE: Value does not exist in theme
+            }),
+          },
+          Text: {
+            style: () => ({
+              fontSize: scale550,
+              lineHeight: scale700,
+              fontWeight: 400,
+              color: dark1,
+            }),
+          },
+          Action: {
+            style: () => ({
+              marginLeft: scale100,
+              marginTop: scale0,
+            }),
+          },
+          ActionIcon: {
+            style: () => ({
+              height: scale400,
+              width: scale400,
+              color: dark1,
+            }),
+          },
+        }),
       }}
     >
       {value}
-    </ParagraphSmall>
+    </TagComponent>
   );
 };
