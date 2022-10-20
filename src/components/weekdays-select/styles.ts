6bfe57694bfa6c199ff29d636449129cdb6e6117
danylo-hlibov-timechimp <d.hlibov@timechimp.com>
2022-10-20 14:10:56 +0300

    feat(CSR-3314): weekdays select

---
+++ src/components/weekdays-select/styles.ts
@@ -0,0 +1,47 @@
+import { CustomThemeType } from 'models';
+import { themedStyled } from 'theme';
+import { borderRadius } from 'utils';
+import { StyledWeekdayProps } from './types';
+
+const getBackgroundColor = ({ $isDisable, $active, $theme }: StyledWeekdayProps & { $theme: CustomThemeType }) => {
+  if ($isDisable) {
+    return $theme.customColors.light3;
+  }
+  if ($active) {
+    return $theme.customColors.primaryLighter;
+  }
+  return $theme.customColors.light4;
+};
+
+export const Container = themedStyled('div', ({ $theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: $theme.sizing.scale600,
+}));
+
+export const WeekdaysContainer = themedStyled('div', ({ $theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: $theme.sizing.scale400,
+}));
+
+export const SelectAllLink = themedStyled('div', () => ({
+  textDecoration: 'underline',
+}));
+
+export const Weekday = themedStyled<'div', StyledWeekdayProps>('div', ({ $theme, $active, $isDisable }) => {
+  const borderColor = !$active || $isDisable ? $theme.customColors.light2 : $theme.customColors.primaryMain;
+  return {
+    backgroundColor: getBackgroundColor({ $active, $isDisable, $theme }),
+    boxSizing: 'border-box',
+    fontSize: '14px',
+    height: $theme.sizing.scale1000,
+    width: $theme.sizing.scale1000,
+    border: `1px solid ${borderColor}`,
+    ...borderRadius($theme.sizing.scale1000),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: $isDisable ? 'not-allowed' : 'pointer',
+  };
+});
---
+++ src/components/weekdays-select/types.ts
@@ -0,0 +1,22 @@
+import { Dispatch, SetStateAction } from 'react';
+
+export interface WeekdaysSelectProps {
+  weekDays: Weekday[];
+  selectedWeekDays: string[];
+  selectWeekDay: (weekDay: string) => void;
+  setSelectedWeekdays?: Dispatch<SetStateAction<string[]>>;
+  withSelectAll?: boolean;
+  disabledDays?: string[];
+  selectAllLabel?: string;
+  selectAllWithDisabled?: boolean;
+}
+
+export interface StyledWeekdayProps {
+  $active: boolean;
+  $isDisable: boolean;
+}
+
+export interface Weekday {
+  id: string;
+  label: string;
+}
