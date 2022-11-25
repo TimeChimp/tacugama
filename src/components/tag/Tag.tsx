a978741cfe9e4e09511726f172938e5ff2542ae6
Hidde Ottens <hidde95@gmail.com>
2022-11-25 15:26:52 +0100

    fix: tag component styling (#880)
    * fix: tag component styling
    
    * fix: import path

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 import { Tag as TagComponent, TagProps as TagComponentProps } from 'baseui/tag';
 import { useTheme } from '../../providers';
 import { border, borderRadius, margin, padding } from '../../utils';
+import { ParagraphSmall, ParagraphXSmall } from '../typography';
 
 export enum TagSize {
   small = 'small',
@@ -20,7 +21,7 @@ export const Tag = ({ value, size = TagSize.small, closeable = false, cursor = '
   const {
     theme: {
       current: {
-        sizing: { scale0, scale100, scale200, scale300, scale400, scale500, scale550, scale700 },
+        sizing: { scale100, scale200, scale400, scale750 },
         borders: { border300 },
         customColors: { light2, dark1 },
       },
@@ -46,15 +47,11 @@ export const Tag = ({ value, size = TagSize.small, closeable = false, cursor = '
               }),
               backgroundColor: '#F9FAFB', // NOTE: Value does not exist in theme
               cursor,
+              height: scale750,
             }),
           },
           Text: {
-            style: () => ({
-              fontSize: scale500,
-              lineHeight: scale700,
-              fontWeight: 400,
-              color: dark1,
-            }),
+            component: ParagraphXSmall,
           },
           Action: {
             style: () => ({
@@ -73,8 +70,8 @@ export const Tag = ({ value, size = TagSize.small, closeable = false, cursor = '
           Root: {
             style: () => ({
               display: 'inline-flex',
-              height: 'auto',
-              ...padding(scale0, scale300),
+              height: scale750,
+              ...padding('0', scale200),
               ...borderRadius(scale100),
               ...margin('0px', scale100, '0px', '0px'),
               ...border({
@@ -86,12 +83,7 @@ export const Tag = ({ value, size = TagSize.small, closeable = false, cursor = '
             }),
           },
           Text: {
-            style: () => ({
-              fontSize: scale550,
-              lineHeight: scale700,
-              fontWeight: 400,
-              color: dark1,
-            }),
+            component: ParagraphSmall,
           },
           Action: {
             style: () => ({
