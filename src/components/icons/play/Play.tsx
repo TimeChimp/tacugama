4ac4382352591e872954cb323f4c503928e5f8da
danylo-hlibov-timechimp <d.hlibov@timechimp.com>
2022-12-15 12:31:16 +0200

    feat(CSR-3499): added new icons

---
+++ src/components/icons/play/Play.tsx
@@ -0,0 +1,19 @@
+import { DEFAULT_ICON_SIZE } from '../.././../models';
+import React from 'react';
+import { useTheme } from '../../../providers';
+import { Icon, SVGProps } from '../..';
+
+export const PlayIcon = ({ title = 'Play', size = DEFAULT_ICON_SIZE, color }: SVGProps) => {
+  const { theme } = useTheme();
+
+  return (
+    <Icon title={title}>
+      <svg width={size} height={size} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M12.0003 7.9067V8.09336C11.9998 8.27107 11.9062 8.43551 11.7537 8.5267L5.78699 12C5.42699 12.2134 5.24033 12.2134 5.08033 12.12L4.91366 12.0267C4.76512 11.938 4.67211 11.7796 4.66699 11.6067V4.39336C4.66753 4.21566 4.76113 4.05122 4.91366 3.96003L5.08033 3.8667C5.24033 3.77336 5.42699 3.77336 5.89366 4.0467L11.7537 7.47336C11.9062 7.56456 11.9998 7.72899 12.0003 7.9067Z"
+          fill={color || theme.current.customColors.dark1}
+        />
+      </svg>
+    </Icon>
+  );
+};
---
+++ src/components/icons/timesheet/Timesheet.tsx
@@ -0,0 +1,21 @@
+import { DEFAULT_ICON_SIZE } from '../.././../models';
+import React from 'react';
+import { useTheme } from '../../../providers';
+import { Icon, SVGProps } from '../..';
+
+export const TimesheetIcon = ({ title = 'Timesheet', size = DEFAULT_ICON_SIZE, color }: SVGProps) => {
+  const { theme } = useTheme();
+
+  return (
+    <Icon title={title}>
+      <svg width={size} height={size} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M2.66634 2.66675H13.333C14.0694 2.66675 14.6663 3.2637 14.6663 4.00008V12.0001C14.6663 12.7365 14.0694 13.3334 13.333 13.3334H2.66634C1.92996 13.3334 1.33301 12.7365 1.33301 12.0001V4.00008C1.33301 3.2637 1.92996 2.66675 2.66634 2.66675ZM10.6663 4.00008V6.00008H13.333V4.00008H10.6663ZM6.49967 6.00008V4.00008H9.49967V6.00008H6.49967ZM2.66634 4.00008V6.00008H5.33301V4.00008H2.66634ZM10.6663 8.83341V7.16675H13.333V8.83341H10.6663ZM6.49967 7.16675V8.83341H9.49967V7.16675H6.49967ZM2.66634 8.83341V7.16675H5.33301V8.83341H2.66634ZM10.6663 10.0001V12.0001H13.333V10.0001H10.6663ZM6.49967 12.0001V10.0001H9.49967V12.0001H6.49967ZM2.66634 10.0001V12.0001H5.33301V10.0001H2.66634Z"
+          fill={color || theme.current.customColors.dark1}
+        />
+      </svg>
+    </Icon>
+  );
+};
