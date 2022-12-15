e1328171db9f095a3ebe4f81223babe828f30194
bohdan-timechimp <93131695+bohdan-timechimp@users.noreply.github.com>
2022-12-15 17:01:53 +0200

    fix(CSR-602): added ability to pass date format to graph tooltip (#709)
    * fix(CSR-1947): updated Bar graph item label
    
    * 0.28.2
    
    * fix(CSR-1947): small graph updates (#592)
    
    * 0.29.0
    
    * axios data duplication prevented (#593)
    
    * 0.29.1
    
    * fix(CSR-1935): added ability to pass params to Line Graph (#594)
    
    * 0.29.3
    
    * feat(CSR-1946): add graph tooltip (#595)
    
    * feat(CSR-1946): add graph tooltip
    
    * increase size limit
    
    * remove size job
    
    * Feat/csr 1946/tooltip (#596)
    
    * feat(CSR-1946): add graph tooltip
    
    * increase size limit
    
    * remove size job
    
    * add params for h symbol
    
    * 0.29.5
    
    * fix flex-grid item (#598)
    
    * 0.29.6
    
    * fix: update toggle story (#600)
    
    * 0.30.0
    
    * fix(CSR-1947): made bars evenly displayed (#603)
    
    * 0.30.2
    
    * fix(CSR-1947): reduced Bar graph barRatio param (#605)
    
    * fix(CSR-1947): reduced Bar graph barRatio param
    
    * fix: bump package version
    
    * feat(CSR-2022): added new unarchive icon (#602)
    
    * feat: feat(CSR-2022): added new unarchive icon
    
    * fix: fixed end of line
    
    * fix: fixed version
    
    * fix: fixed end of file
    
    * 0.30.6
    
    * Feat/csr 2023/done icon (#606)
    
    * feat(CSR-2023) done icon
    
    * fix(CSR-1947): added handling for single bar (#607)
    
    * fix(CSR-1947): added handling for single bar
    
    * fix: bump package version
    
    * 0.30.9
    
    * 0.30.10
    
    * new user invited icon (#608)
    
    * new user invited icon
    
    * 0.30.11
    
    * add some fixes to tooltip after review (#609)
    
    * add ability to pass translations (#610)
    
    * 0.30.13
    
    * feat(CSR-2021): added overrides to Radio Item (#611)
    
    * 0.30.15
    
    * fix tooltip margins (#612)
    
    * 0.30.16
    
    * 0.30.17
    
    * feat(CSR-2057): added Limit and Progress lines to Line Graph (#614)
    
    * 0.30.19
    
    * keep tooltip in screenbox (#615)
    
    * keep tooltip in screenbox
    
    * fix dependencies
    
    * 0.30.20
    
    * feat(CSR-2072): fixed price tooltip (#617)
    
    * feat(CSR-2072): fixed price tooltip
    
    * fix: fixed version
    
    * fixed: prettier problems
    
    * 0.30.26
    
    * fix(CSR-2057): fixed not displayed additional line graph lines (#623)
    
    * fix(CSR-2057): fixed not displayed additional line graph lines
    
    * fix: updated lock file
    
    * fix: fixed lock file
    
    Co-authored-by: Olga <ohalytska@griddynamics.com>
    
    * 0.30.28
    
    * feat/CSR-2036/add-on-select-handler (#624)
    
    * fix(CSR-602): added ability to pass date format to graph tooltip
    
    * fix: fixed errors
    
    * fix: updated props
    
    * fix: removed unused lines
    
    Co-authored-by: Hidde Ottens <hidde95@gmail.com>
    Co-authored-by: Ihor Lavrov <40561417+Ihor-Lavrov@users.noreply.github.com>
    Co-authored-by: ihor-lavrov <ihor.lavrov.2018@gmail.com>
    Co-authored-by: Olha <77673057+ohalytska-ui@users.noreply.github.com>
    Co-authored-by: Olga <ohalytska@griddynamics.com>

--- src/components/graphs/line/LineGraph.tsx
+++ src/components/graphs/line/LineGraph.tsx
@@ -46,6 +46,7 @@ export interface LineGraphProps {
   flyOutWidth?: number;
   flyOutHeight?: number;
   legendData?: LegendData[];
+  dateFormat?: string;
 }
 
 export const LineGraph = ({
@@ -68,11 +69,12 @@ export const LineGraph = ({
   flyOutWidth = 270,
   flyOutHeight = 200,
   legendData,
+  dateFormat,
 }: LineGraphProps) => {
   const {
     theme: {
       current: {
-        customColors: { primarySubtle, dark0, dark4, light2, purple3, purple2 },
+        customColors: { primarySubtle, dark0, dark4, light2, purple2, purple3 },
         sizing: { scale400, scale600 },
       },
     },
@@ -169,6 +171,7 @@ export const LineGraph = ({
                   flyOutTooltipHeight={flyOutHeight}
                   formatAsDate={formatAsDate}
                   horizontalAxisItemLabel={horizontalAxisItemLabel}
+                  dateFormat={dateFormat}
                 />
               }
             />
