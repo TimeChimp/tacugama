b283a1dbf4834d38741d2e8a98bf1c58e8cf33fe
Olha <77673057+ohalytska-ui@users.noreply.github.com>
2022-12-16 14:40:04 +0100

    feat(CSR-3503): ui refactor avatar (#896)
    * feat(CSR-3503): ui refactor avatar
    
    * fix: fixed imports for tests
    
    * fix: fixed tacu version
    
    Co-authored-by: Hidde Ottens <hidde95@gmail.com>

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,45 +1,96 @@
 import React from 'react';
 import { Avatar as BaseAvatar, AvatarProps as BaseAvatarProps } from 'baseui/avatar';
-import { Theme } from 'baseui/theme';
-import { DATA_TEST_ID } from '../../models';
+import { AvatarType, DATA_TEST_ID } from '../../models';
+import { useTheme } from '../../providers';
+import { AvatarWithIcon } from './styles';
+import { border } from '../../utils';
 
 export interface AvatarProps extends BaseAvatarProps {
   testId?: string;
+  icon?: JSX.Element;
+  type?: AvatarType;
 }
 
-export const Avatar = ({ testId, size, ...rest }: AvatarProps) => {
-  const getFontSize = ($theme: Theme) => {
+export const Avatar = ({ testId, icon, size, type, ...rest }: AvatarProps) => {
+  const {
+    theme: {
+      current: {
+        sizing: { scale400, scale500, scale800, scale1000 },
+        colors: { primary, primaryA },
+        customColors: { dark1, light3 },
+        borders: { border300 },
+      },
+    },
+  } = useTheme();
+
+  const getFontSize = () => {
     if (size) {
       const sizeNumber = parseInt(size, 10);
-      const defaultSize = parseInt($theme.sizing.scale1000, 10);
+      const defaultSize = parseInt(scale1000, 10);
 
       if (sizeNumber > defaultSize) {
-        return $theme.sizing.scale800;
+        return scale400;
       }
     }
-    return $theme.sizing.scale550;
+    return scale500;
+  };
+
+  const avatarOverrides = () => {
+    switch (type) {
+      case AvatarType.default:
+        return {
+          Root: {
+            style: {
+              backgroundColor: primary,
+              height: scale800,
+              width: scale800,
+            },
+            props: {
+              [DATA_TEST_ID]: testId,
+            },
+          },
+          Initials: {
+            style: {
+              fontSize: getFontSize(),
+              color: primaryA,
+            },
+          },
+        };
+      case AvatarType.dashed:
+        return {
+          Root: {
+            style: {
+              backgroundColor: light3,
+              height: scale800,
+              width: scale800,
+              borderRadius: '50%',
+              ...border({
+                ...border300,
+                borderColor: dark1,
+                borderStyle: 'dashed',
+              }),
+            },
+            props: {
+              [DATA_TEST_ID]: testId,
+            },
+          },
+          Initials: {
+            style: {
+              fontSize: getFontSize(),
+              color: dark1,
+            },
+          },
+        };
+    }
   };
 
   return (
-    <BaseAvatar
-      overrides={{
-        Root: {
-          style: ({ $theme }) => ({
-            backgroundColor: $theme.colors.primary,
-          }),
-          props: {
-            [DATA_TEST_ID]: testId,
-          },
-        },
-        Initials: {
-          style: ({ $theme }) => ({
-            fontSize: getFontSize($theme),
-            color: $theme.colors.primaryA,
-          }),
-        },
-      }}
-      {...rest}
-      size={size}
-    />
+    <>
+      {icon ? (
+        <AvatarWithIcon $type={type}>{icon}</AvatarWithIcon>
+      ) : (
+        <BaseAvatar overrides={avatarOverrides()} {...rest} size={size} />
+      )}
+    </>
   );
 };
