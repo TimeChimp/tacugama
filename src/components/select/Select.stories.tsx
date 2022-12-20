fbe017b2f44d40fd82b9ecfd7ced3aa23aeb1dc7
danylo-hlibov-timechimp <91881370+danylo-hlibov-timechimp@users.noreply.github.com>
2022-12-20 11:08:23 +0100

    feat(CSR-3495): refactoring select component (#908)
    * feat(CSR-3495): refactoring select component
    
    * fix: fixed version

--- src/components/select/Select.stories.tsx
+++ src/components/select/Select.stories.tsx
@@ -12,32 +12,69 @@ export default {
 
 const Template: Story<SelectProps> = (args) => {
   const [selectedValue, setSelectedValue] = useState<Value>([]);
-  return <Select {...args} onChangeHandler={({ value }) => setSelectedValue(value)} value={selectedValue} />;
+  return (
+    <Select
+      {...args}
+      onChangeHandler={({ option }) => setSelectedValue(option ? [option] : [])}
+      value={selectedValue}
+    />
+  );
 };
+Template.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/QrIqXt997mm9ePey5JCLAJ/DS-1.0?node-id=1733%3A11543&t=NbmXDJ7K9Ilt6afV-4',
+  },
+};
+
 const ColorSelectTemplate: Story<ColorSelectProps> = (args) => <ColorSelect {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   options: OPTIONS,
-  placeholder: 'Select color',
+  placeholder: 'Placeholder',
+};
+Default.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/QrIqXt997mm9ePey5JCLAJ/DS-1.0?node-id=1733%3A11543&t=NbmXDJ7K9Ilt6afV-4',
+  },
 };
 
 export const Multi = Template.bind({});
 Multi.args = {
   options: OPTIONS,
-  placeholder: 'Select color',
+  placeholder: 'Placeholder',
   multi: true,
+};
+Multi.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/QrIqXt997mm9ePey5JCLAJ/DS-1.0?node-id=1733%3A11543&t=NbmXDJ7K9Ilt6afV-4',
+  },
 };
 
 export const Loading = Template.bind({});
 Loading.args = {
   options: OPTIONS,
-  placeholder: 'Select color',
+  placeholder: 'Placeholder',
   isLoading: true,
+};
+Loading.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/QrIqXt997mm9ePey5JCLAJ/DS-1.0?node-id=1733%3A11543&t=NbmXDJ7K9Ilt6afV-4',
+  },
 };
 
 export const Color = ColorSelectTemplate.bind({});
 Color.args = {
-  placeholder: 'Select color',
+  placeholder: 'Placeholder',
   colors: OPTIONS,
+};
+Color.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/QrIqXt997mm9ePey5JCLAJ/DS-1.0?node-id=1733%3A11543&t=NbmXDJ7K9Ilt6afV-4',
+  },
 };
