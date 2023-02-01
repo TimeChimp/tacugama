e522903d264559fb10364b8ef8eef419e2970ad0
Hidde Ottens <hidde95@gmail.com>
2023-02-01 10:06:56 +0100

    fix: progress bar props

--- src/components/progress-bar/ProgressBar.tsx
+++ src/components/progress-bar/ProgressBar.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { ProgressBar as BaseProgressBar } from 'baseui/progress-bar';
 import { ProgressBarProps } from './types';
+import { margin } from '../../utils';
 
-export const ProgressBar = (props: Partial<ProgressBarProps>) => <BaseProgressBar {...props} overrides={{
-    BarProgress: {
+export const ProgressBar = (props: ProgressBarProps) => (
+  <BaseProgressBar
+    {...props}
+    overrides={{
+      BarProgress: {
         style: ({ $theme }) => ({
-            backgroundColor: $theme.colors.primary,
+          backgroundColor: $theme.colors.primary,
         }),
-    },
-}} />;
+      },
+      BarContainer: { style: margin('0') },
+    }}
+  />
+);
