e24a3f92fb465f2bdd2ef6eafd82eaac5252dbca
Olha <77673057+ohalytska-ui@users.noreply.github.com>
2023-03-29 00:18:55 +0200

    fix(CSR-3838): fixed-tracking-time-select-button-onClick (#1022)
    * fix/CSR-3838/fixed-tracking-time-select-button-onClick
    
    * fix: removed unused

--- src/components/select/styles.ts
+++ src/components/select/styles.ts
@@ -1,14 +1,11 @@
 import { themedStyled } from '../../theme';
 import { borderRadius } from '../../utils';
 
-export interface DropdownButtonWrapperProps {
-  $width?: string;
-}
-
-export const DropdownButtonWrapper = themedStyled<'div', DropdownButtonWrapperProps>('div', ({ $theme, $width }) => ({
+export const DropdownButtonWrapper = themedStyled('div', ({ $theme }) => ({
   height: $theme.sizing.scale1000,
   display: 'flex',
   justifyContent: 'start',
+  alignItems: 'center',
   backgroundColor: $theme.colors.primaryB,
   cursor: 'pointer',
   paddingLeft: $theme.sizing.scale600,
