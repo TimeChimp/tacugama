1426be96416b10b29a436a86d12f80a420f1c441
ohalytska-ui <77673057+ohalytska-ui@users.noreply.github.com>
2023-03-28 11:04:39 +0200

    fix: fixed sizing

--- src/components/select/styles.ts
+++ src/components/select/styles.ts
@@ -7,13 +7,13 @@ export interface DropdownButtonWrapperProps {
 
 export const DropdownButtonWrapper = themedStyled<'div', DropdownButtonWrapperProps>('div', ({ $theme, $width }) => ({
   width: $width,
-  height: '40px',
+  height: $theme.sizing.scale1000,
   display: 'flex',
   justifyContent: 'start',
   position: 'absolute',
   top: '99%',
   backgroundColor: $theme.colors.primaryB,
   paddingLeft: $theme.sizing.scale600,
-  boxShadow: 'rgba(0, 0, 0, 0.16) 0px 4px 16px',
+  boxShadow: `rgba(0, 0, 0, 0.16) 0px ${$theme.sizing.scale100} ${$theme.sizing.scale600}`,
   ...borderRadius($theme.borders.radius200),
 }));
