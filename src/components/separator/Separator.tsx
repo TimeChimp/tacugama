ebe29b2df3cf04cb01b47aa254a2f6cca5d547a3
Bohdan Burukhin <b.buruhkin@timechimp.com>
2023-03-29 08:51:52 +0300

    feat/Added margin prop to Separator component

--- src/components/separator/Separator.tsx
+++ src/components/separator/Separator.tsx
@@ -2,4 +2,6 @@ import React from 'react';
 import { Seperator as SeparatorComponent } from './styles';
 import { SeparatorProps } from './types';
 
-export const Separator = ({ noMargin }: SeparatorProps) => <SeparatorComponent $noMargin={noMargin} />;
+export const Separator = ({ noMargin, margin }: SeparatorProps) => (
+  <SeparatorComponent $noMargin={noMargin} $margin={margin} />
+);
