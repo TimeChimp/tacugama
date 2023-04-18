bbd36b9bd3886cbc0f84e50271d3bb5b8c507c4a
Olha <77673057+ohalytska-ui@users.noreply.github.com>
2023-04-18 10:26:33 +0200

    feat(CSR-3861): finance invoices icons (#1044)
    * feat/CSR-3861/finance-invoices-icons
    
    * fix: added ends of line

---
+++ src/components/icons/radio-line/RadioLine.tsx
@@ -0,0 +1,13 @@
+import { DEFAULT_ICON_SIZE } from '../.././../models';
+import React from 'react';
+import { Icon, SVGProps } from '../../icon';
+
+export const RadioLine = ({ title = 'RadioLine', size = DEFAULT_ICON_SIZE, color }: SVGProps) => (
+  <Icon title={title}>
+    <svg width={size} height={size} viewBox="0 0 18 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="7" y="70" width="70" height="4" transform="rotate(-90 7 70)" fill="#CFD6E4" />
+      <circle cx="9" cy="35" r="8.5" fill="white" stroke={color || 'currentColor'} />
+      <circle cx="9" cy="35" r="5" fill={color || 'currentColor'} />
+    </svg>
+  </Icon>
+);
