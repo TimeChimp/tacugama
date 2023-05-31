8fe8d53acb78755a41cf3a778f4ee0855686d402
OksanaTC <o.kukushkina@timechimp.com>
2023-05-31 09:54:12 +0200

    Fix type

--- src/components/movable-table/types.ts
+++ src/components/movable-table/types.ts
@@ -1,11 +1,5 @@
-import { TableProps } from 'baseui/table';
-import { BasicTableColumn, BasicTableRow } from 'components/basic-table';
+import { BasicTableProps } from 'components/basic-table';
 
-type OmittedTableProps = 'columns' | 'data';
-
-export interface MovableTableProps extends Omit<TableProps, OmittedTableProps> {
-  columns: BasicTableColumn[];
-  data: BasicTableRow[];
-  setData?: (data: BasicTableRow[]) => void;
+export interface MovableTableProps extends BasicTableProps {
   entityRows: any[];
 }
