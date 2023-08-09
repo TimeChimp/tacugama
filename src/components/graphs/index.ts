ce50ff915908d4f68929aa10a40f7d17d3228d8d
Hidde Ottens <hidde95@gmail.com>
2023-08-09 09:36:27 +0200

    chore: limit victory export

--- src/components/graphs/index.ts
+++ src/components/graphs/index.ts
@@ -2,4 +2,15 @@ export * from './line';
 export * from './bar';
 export * from './myDashboardBar';
 export * from './utils';
-export * from 'victory';
+export {
+  VictoryAxis,
+  VictoryBar,
+  VictoryChart,
+  VictoryTheme,
+  VictoryTooltip,
+  VictoryArea,
+  VictoryLegend,
+  VictoryScatter,
+  VictoryVoronoiContainer,
+  VictoryStack,
+} from 'victory';
