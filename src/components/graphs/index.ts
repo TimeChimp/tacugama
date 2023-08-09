b598190dc65af4198a9172e29d481ac600c96d49
Hidde Ottens <hidde95@gmail.com>
2023-08-09 09:43:14 +0200

    feat: export victory components from tacugama (#1188)
    * feat: export victory componets from tacugama
    
    * chore: limit victory export

--- src/components/graphs/index.ts
+++ src/components/graphs/index.ts
@@ -2,3 +2,15 @@ export * from './line';
 export * from './bar';
 export * from './myDashboardBar';
 export * from './utils';
+export {
+  VictoryAxis,
+  VictoryBar,
+  VictoryChart,
+  VictoryTheme,
+  VictoryTooltip,
+  VictoryArea,
+  VictoryLegend,
+  VictoryScatter,
+  VictoryVoronoiContainer,
+  VictoryStack,
+} from 'victory';
