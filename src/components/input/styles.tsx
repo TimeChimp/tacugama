21209330b5ba2723aff5d0926e783c33d605c97f
Hidde Ottens <hidde95@gmail.com>
2023-09-04 11:15:06 +0200

    chore: fix color input width

--- src/components/input/styles.tsx
+++ src/components/input/styles.tsx
@@ -26,5 +26,6 @@ export const ColorPickerContainer = themedStyled('div', () => {
 export const ColorPickerWrapper = themedStyled('div', () => {
   return {
     position: 'relative',
+    width: '100%',
   };
 });
