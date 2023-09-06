0359a4681964210186c3e39d8b49a43936e50413
Hidde Ottens <hidde95@gmail.com>
2023-09-06 11:29:56 +0200

    feat: update font weights (#1232)

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -6,48 +6,48 @@ export const typography: DeepPartial<Theme['typography']> = {
   HeadingXXLarge: {
     fontSize: '40px',
     lineHeight: '52px',
-    fontWeight: 400,
+    fontWeight: 500,
   },
   HeadingSmall: {
     fontSize: '24px',
     lineHeight: '32px',
-    fontWeight: 400,
+    fontWeight: 500,
   },
   HeadingXSmall: {
     fontSize: '20px',
     lineHeight: '28px',
-    fontWeight: 400,
+    fontWeight: 500,
   },
   /* Labels */
   LabelMedium: {
     fontSize: '16px',
     lineHeight: '20px',
-    fontWeight: 400,
+    fontWeight: 500,
   },
   LabelSmall: {
     fontSize: '14px',
     lineHeight: '20px',
-    fontWeight: 400,
+    fontWeight: 500,
   },
   LabelXSmall: {
     fontSize: '12px',
     lineHeight: '20px',
-    fontWeight: 400,
+    fontWeight: 500,
   },
   /* Paragraph */
   ParagraphMedium: {
     fontSize: '16px',
     lineHeight: '24px',
-    fontWeight: 'normal',
+    fontWeight: 400,
   },
   ParagraphSmall: {
     fontSize: '14px',
     lineHeight: '20px',
-    fontWeight: 'normal',
+    fontWeight: 400,
   },
   ParagraphXSmall: {
     fontSize: '12px',
     lineHeight: '20px',
-    fontWeight: 'normal',
+    fontWeight: 400,
   },
 };
