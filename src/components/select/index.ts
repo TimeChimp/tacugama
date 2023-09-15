164ea3a85a62a4bead5e73639ca5c377bf0a1b00
Hidde Ottens <hidde95@gmail.com>
2023-09-15 16:32:51 +0200

    fix: add missing option export (#1251)

--- src/components/select/index.ts
+++ src/components/select/index.ts
@@ -2,3 +2,4 @@ export * from './multi-select';
 export * from './single-select/SingleSelect';
 export { Option as SingleSelectOption } from './single-select/types';
 export * from './types';
+export { Option } from 'baseui/select';
