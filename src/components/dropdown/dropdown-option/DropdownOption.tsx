4916ba623c34b4cb955eaf2aa841ca709f49824d
oksana-timeChimp <127097222+OksanaTC@users.noreply.github.com>
2023-10-25 09:35:41 +0200

    [CSR-4776] Fix tooltip (#1313)
    * Fix tooltip size
    
    * Change version
    
    * Fix tooltip

--- src/components/dropdown/dropdown-option/DropdownOption.tsx
+++ src/components/dropdown/dropdown-option/DropdownOption.tsx
@@ -9,61 +9,35 @@ import {
   StyledDropdownOptionLabel,
 } from '../styles';
 import { DropdownOptionProps } from '../types';
-import { StatefulTooltip } from '../../tooltip';
-import { HIGH_Z_INDEX } from '../../../models';
 
 export const DropdownOption = forwardRef<any, DropdownOptionProps>(
   ({ item, onItemSelect }: DropdownOptionProps, ref) => {
     const { theme } = useTheme();
-    const {
-      current: {
-        sizing: { scale400, scale500 },
-        customSizing: { scale4375 },
-      },
-    } = theme;
 
     return (
-      <StatefulTooltip
-        placement="top"
-        showArrow
-        triggerType="hover"
-        overrides={{
-          Body: {
-            style: {
-              fontSize: scale400,
-              lineHeight: scale500,
-              zIndex: HIGH_Z_INDEX,
-              width: 'fit-content',
-              maxWidth: scale4375,
-              wordBreak: 'break-all',
-            },
-          },
-        }}
-        content={item.showTooltip ? item.label : ''}
-      >
-        <StyledDropdownOption ref={ref} onClick={() => onItemSelect(item)}>
-          {item.checkbox && <Checkbox testId="dropdown-option-checkbox" checked={item.isChecked} />}
-          {item.icon && <StyledDropdownOptionIcon>{item.icon}</StyledDropdownOptionIcon>}
-          <StyledDropdownOptionLabel>
-            <ParagraphSmall
-              color={item.color || theme.current.colors.contentPrimary}
-              overrides={{
-                Block: {
-                  style: {
-                    fontWeight: item.isBold ? 600 : 400,
-                    whiteSpace: 'nowrap',
-                    textOverflow: 'ellipsis',
-                    overflow: 'hidden',
-                  },
+      <StyledDropdownOption ref={ref} onClick={() => onItemSelect(item)}>
+        {item.checkbox && <Checkbox testId="dropdown-option-checkbox" checked={item.isChecked} />}
+        {item.icon && <StyledDropdownOptionIcon>{item.icon}</StyledDropdownOptionIcon>}
+        <StyledDropdownOptionLabel>
+          <ParagraphSmall
+            title={item.label}
+            color={item.color || theme.current.colors.contentPrimary}
+            overrides={{
+              Block: {
+                style: {
+                  fontWeight: item.isBold ? 600 : 400,
+                  whiteSpace: 'nowrap',
+                  textOverflow: 'ellipsis',
+                  overflow: 'hidden',
                 },
-              }}
-            >
-              {item.label}
-            </ParagraphSmall>
-            {item.iconEnd && <StyledDropdownOptionIconEnd>{item.iconEnd}</StyledDropdownOptionIconEnd>}
-          </StyledDropdownOptionLabel>
-        </StyledDropdownOption>
-      </StatefulTooltip>
+              },
+            }}
+          >
+            {item.label}
+          </ParagraphSmall>
+          {item.iconEnd && <StyledDropdownOptionIconEnd>{item.iconEnd}</StyledDropdownOptionIconEnd>}
+        </StyledDropdownOptionLabel>
+      </StyledDropdownOption>
     );
   },
 );
