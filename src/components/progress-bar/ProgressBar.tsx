7ccd293a7cfbf3935b48cf08185411843aa259ce
Hidde Ottens <hidde95@gmail.com>
2023-11-17 15:44:33 +0100

    fix: show red progress when > 100 (#1358)

--- src/components/progress-bar/ProgressBar.tsx
+++ src/components/progress-bar/ProgressBar.tsx
@@ -3,13 +3,14 @@ import { ProgressBar as BaseProgressBar } from 'baseui/progress-bar';
 import { ProgressBarProps } from './types';
 import { margin } from '../../utils';
 
-export const ProgressBar = (props: ProgressBarProps) => (
+export const ProgressBar = ({ value, ...rest }: ProgressBarProps) => (
   <BaseProgressBar
-    {...props}
+    value={value}
+    {...rest}
     overrides={{
       BarProgress: {
         style: ({ $theme }) => ({
-          backgroundColor: $theme.colors.primary,
+          backgroundColor: value && value > 100 ? $theme.customColors.red0 : $theme.colors.primary,
         }),
       },
       BarContainer: { style: margin('0') },
