a1e602794d416ba67ef11f002af69580e556231e
Hidde Ottens <hidde95@gmail.com>
2024-02-20 12:02:31 +0100

    chore: update form group styling

--- src/components/form-group/styles.ts
+++ src/components/form-group/styles.ts
@@ -3,7 +3,7 @@ import { themedStyled } from '../../theme';
 export const FormGroupStack = themedStyled('div', ({ $theme }) => ({
   display: 'flex',
   flexDirection: 'column',
-  gap: $theme.sizing.scale500,
+  gap: $theme.sizing.scale750,
 }));
 
 export const FormGroupTitleStack = themedStyled('div', ({ $theme }) => ({
