9ad65bc03bb632034cb108473543cca1867b484c
Wouter Baltus <wouterbaltus@hotmail.com>
2024-11-29 11:54:47 +0100

    Added extra options for data grid export

--- src/components/data-grid/export/exportCSV.ts
+++ src/components/data-grid/export/exportCSV.ts
@@ -1,13 +1,13 @@
 import { GridApi } from '@ag-grid-community/core';
 import { generateFilename } from '../../../utils';
 
-export const exportCSV = (gridApi: GridApi, fileName?: string) => {
+export const exportCSV = (gridApi: GridApi, fileName?: string, exportColumnKeys?: string[]) => {
   const allDisplayedColumns = gridApi.getAllDisplayedColumns();
   const columns = allDisplayedColumns?.map((displayedColumnn) => displayedColumnn.getColDef());
 
   gridApi.exportDataAsCsv({
     fileName: fileName ?? generateFilename(gridApi),
     onlySelectedAllPages: true,
-    columnKeys: columns?.map((column) => column.colId ?? column.field) as string[],
+    columnKeys: exportColumnKeys ?? (columns?.map((column) => column.colId ?? column.field) as string[]),
   });
 };
--- src/components/data-grid/export/exportExcel.ts
+++ src/components/data-grid/export/exportExcel.ts
@@ -1,13 +1,13 @@
 import { GridApi } from '@ag-grid-community/core';
 import { generateFilename } from '../../../utils';
 
-export const exportExcel = (gridApi: GridApi, fileName?: string) => {
+export const exportExcel = (gridApi: GridApi, fileName?: string, exportColumnKeys?: string[]) => {
   const allDisplayedColumns = gridApi.getAllDisplayedColumns();
   const columns = allDisplayedColumns?.map((displayedColumnn) => displayedColumnn.getColDef());
 
   gridApi.exportDataAsExcel({
     fileName: fileName ?? generateFilename(gridApi),
     onlySelectedAllPages: true,
-    columnKeys: columns?.map((column) => column.colId ?? column.field) as string[],
+    columnKeys: exportColumnKeys ?? (columns?.map((column) => column.colId ?? column.field) as string[]),
   });
 };
